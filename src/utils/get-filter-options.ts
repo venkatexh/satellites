04b9f536f79e75874772282ee62c561b16958fe7
Venkatesh Chakrabarty <venkateshchakrabarty@gmail.com>
2023-07-20 20:32:39 +0530

    Filter out empty options
    Signed-off-by: Venkatesh Chakrabarty <venkateshchakrabarty@gmail.com>

--- src/utils/get-filter-options.ts
+++ src/utils/get-filter-options.ts
@@ -3,18 +3,24 @@ import satellitesData from '../satellites.json'
 
 export const countryCodeOptions = [
   ...new Set(
-    (satellitesData as Satellite[]).filter((obj) => obj !== null).map((obj) => obj.countryCode)
+    (satellitesData as Satellite[])
+      .filter((satellite) => satellite !== null && satellite.countryCode !== null)
+      .map((country) => country.countryCode)
   )
 ]
 
 export const orbitRegimeOptions = [
   ...new Set(
-    (satellitesData as Satellite[]).filter((obj) => obj !== null).map((obj) => obj.orbitCode)
+    (satellitesData as Satellite[])
+      .filter((satellite) => satellite !== null && satellite.orbitCode !== null)
+      .map((obj) => obj.orbitCode)
   )
 ]
 
 export const objectTypeOptions = [
   ...new Set(
-    (satellitesData as Satellite[]).filter((obj) => obj !== null).map((obj) => obj.objectType)
+    (satellitesData as Satellite[])
+      .filter((satellite) => satellite !== null && satellite.objectType !== null)
+      .map((obj) => obj.objectType)
   )
 ]
